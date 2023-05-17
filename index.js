8aabe53e4b9c9004afa2f0e9c376b1f360a390f3
abadat <abadat26@gmail.com>
2023-05-18 02:50:09 +0600

    completed my deploy vercel and checked ok

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res)=>{
     res.send('Toy Marketplace Server is running')
 })
 
+app.get('/about', (req, res)=>{
+    res.send('About Toy Marketplace')
+})
+
 
 
 app.listen(port, ()=>{
